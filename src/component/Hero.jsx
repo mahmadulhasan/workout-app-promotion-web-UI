1167459ebf19f6a38e1435d0ef8465f1474d96a7
MAHMADUL HASAN <mahmadulhasan623@gmail.com>
2026-04-06 13:18:34 +0530

    2nd push

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,6 +10,14 @@ const Hero = () => {
             <div className="h-[95vh] w-full rounded-[100px] bg-linear-to-r/oklab from-indigo-500 to-teal-400">
                 <p className="text-transparent text-[clamp(1.5rem,10vw,8rem)] w-full text-center font-semibold bg-linear-to-r/oklab from-black via-gray-700 to-black bg-clip-text">Start workout today</p>
                 
+                <div className="w-full flex items-center justify-between">
+                    <img src="/" alt="" className="" />
+                    <img src="" alt="" className="" />
+                </div>
+                <div className="w-full flex items-center justify-between">
+                    <img src="" alt="" className="" />
+                    <img src="" alt="" className="" />
+                </div>
             </div>
 
             <div className="h-[100vh]"></div>
